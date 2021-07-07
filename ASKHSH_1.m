834388cdd7adc7c8c4f88fd92faa1567b841dbb4
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-07-07 03:53:08 +0300

    ασκηση 1.1 και 1.2
    το σημα ειναι τοσο γτπ στο διαστημα -10,10. φαινεται ενα κουτι αν δεν κανεις ζουμ. και με ζουμ φαινεται γτπ, μακαρι να γινοταν ζουμ το χ αλλα οχι το ψ

---
+++ ASKHSH_1.m
@@ -0,0 +1,63 @@
+% Our signal is:
+% x(t) = cos(100*pi*t) + cos(200*pi*t) + sin(500*pi*t)
+% and our domain for the independent variable (t) is [-10,10].
+
+
+
+% 1.Finding the minimum sampling frequency
+%
+% In order to find the minimum sample frequency (f_{s,min}) , we have to 
+% find the maximum frequency implemented in signal.We need to express the 
+% given signal as a superposition of fundamental sinusoidal signals.We can 
+% write the signal as:
+% x(t) = sin(100*pi*t + pi/2) + sin(200*pi*t + pi/2) + sin(500*pi*t)[1]
+% 
+% In general,a signal may be represented by the
+% following mathematical form:
+%
+% x(t) = A * sin(2*pi*f*t+θ)
+%
+% A: denotes the amplitude of the oscillation.
+% F: denotes the frequency of the oscillation.
+% t: denotes the independent time variable.
+% Θ: denotes the phase of the oscillation.
+% 
+% Looking at [1], we can tell that 100*pi = 2*pi*f1, 200*pi = 2*pi*f2 and
+% 500*pi = 2*pi*f3.So we have f1=50hz, f2=100hz and f3=250hz.So the max
+% frequency implemented on signal is fmax=f3=250hz.
+%
+% From the Nyquist Sampling Theorem, we know that:
+% 
+% 1/Ts >= 2*fmax <=> fs >= 2*fmax <=> fs >= 500hz, where Ts is the sample
+% period. So we have f_{s,min} = 500hz
+% 
+% 
+% 2.Representing the signal for -t_max <= t <= t_max where t_max = 10
+% and step dt=0.001
+
+
+dt = 0.001; %step
+t_max = 10; %interval bound (-10 <= t <= 10)
+
+t = -t_max:dt:t_max; %t is a 1*(2*t_max/dt + 1)= 1*20.001 dimension matrix that
+% holds all the time values. t=[-10,-9.999,...,9.999,10]
+
+x = cos(100*pi*t) + cos(200*pi*t) + sin(500*pi*t); %x is a matrix with
+% the same dimensions as t. x holds the signal values, each value
+% calculated as the corresponding x(t).
+% x=[x(t(1)),x(t(2)),...,x(t(20.001))] = [x(-10),x(-9.999),...,x(10)]
+   
+figure('Name','exercise2 signal representation'); %makes a figure window
+%with the corresponding name
+
+plot(t,x,'-r','LineWidth',1.3); %makes x,y axis representing every value in
+%t with its corresponding x(t)
+
+xlabel('-10 \leq t \leq +10');
+ylabel('x(t)'); %x,y labels
+grid on %show grid
+
+
+
+
+
